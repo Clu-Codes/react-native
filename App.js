b5832c0521b968fa88e25c93ec1e2616910a2dc1
Shohan <shohan@shohans-ipro.myfiosgateway.com>
2021-05-29 21:34:57 -0400

    -m

--- App.js
+++ App.js
@@ -1,13 +1,17 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import RedBlock from './assets/red.svg'
 
 export default function App() {
   return (
+  
     <View style={styles.container}>
       <Text>New App, Who Dis?</Text>
       <StatusBar style="auto" />
+      {/* <RedBlock width={48} height={48} fill="#000" /> // no worky...yet */}
     </View>
+
   );
 }
 
--- metro.config.js
+++ metro.config.js
@@ -1,4 +1,23 @@
-// Learn more https://docs.expo.io/guides/customizing-metro
-const { getDefaultConfig } = require('expo/metro-config');
+const { getDefaultConfig } = require('metro-config');
 
-module.exports = getDefaultConfig(__dirname);
+module.exports = (async () => {
+  const {
+    resolver: { sourceExts, assetExts },
+  } = await getDefaultConfig();
+
+  return {
+    transformer: {
+      babelTransformerPath: require.resolve('react-native-svg-transformer'),
+      getTransformOptions: async () => ({
+        transform: {
+          experimentalImportSupport: false,
+          inlineRequires: false,
+        },
+      }),
+    },
+    resolver: {
+      assetExts: assetExts.filter(ext => ext !== 'svg'),
+      sourceExts: [...sourceExts, 'svg'],
+    },
+  };
+})();
